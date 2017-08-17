ef02a42eb4426ff06692c048b3222ccdf6e4ade7
ming5500828 <308043195@qq.com>
2017-08-17 10:08:03 +0800

    创建文件

---
+++ exprice/src/components/layout.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+      <div class="app-header">头部</div>
+      <div class="app-con">
+        <keep-alive>
+             <router-view></router-view>
+        </keep-alive>
+      </div>
+      <div class="app-footer">底部</div>
+  </div>
+</template>
+<script>
+export default {
+  name: 'app'
+}
+</script>
+<style>
+    *{
+        margin:0;
+        padding:0
+    }
+    .app-header{
+        background: red;
+        height:100px;
+    }
+    .app-footer{
+        background: #00ff00;
+        height:200px;
+    }
+</style>
